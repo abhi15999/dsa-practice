f18a0459b810ebd1a15ecface0371a8e1fadf304
Abhishek <abhishek@storyxpress.co>
2022-01-31 10:39:59 +0530

    pascalTriangle

---
+++ Array Part 1/PascalTriangle.py
@@ -0,0 +1,14 @@
+def pascalTriangle():
+    n = int(input())
+    matrix = [[] for x in range(n)]
+    # In var loops
+
+    for i in range(0, n):
+        matrix[i] = [0 for x in range(0, (i + 1))]
+        matrix[i][0] = matrix[i][i] = 1
+
+        for j in range(1, i):
+            matrix[i][j] = matrix[i - 1][j - 1] + matrix[i - 1][j]
+    print(matrix)
+
+pascalTriangle()
